d21db30d761d6340f807611c51adf46244561f06
mairess <trabalho.maires@gmail.com>
2024-03-29 18:43:10 -0300

    feat: implements class ProcessIndustries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,10 +1,17 @@
-from src.insights.jobs import ProcessJobs
 from typing import List
+
+from src.insights.jobs import ProcessJobs
 
 
 class ProcessIndustries(ProcessJobs):
     def __init__(self):
+        self.jobs_list = list()
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        unique_industry = set()
+        for row in self.jobs_list:
+            industry = row["industry"]
+            if industry and industry not in unique_industry:
+                unique_industry.add(industry)
+        return list(unique_industry)
