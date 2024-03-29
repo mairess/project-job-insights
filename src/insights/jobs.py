e2922f10a2b61b87178ec546728d494b5bbcfeda
mairess <trabalho.maires@gmail.com>
2024-03-29 18:36:37 -0300

    feat: implements method get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -12,7 +12,12 @@ class ProcessJobs:
             self.jobs_list = [row for row in content]
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        unique_job = set()
+        for row in self.jobs_list:
+            job_type = row["job_type"]
+            if job_type and job_type not in unique_job:
+                unique_job.add(job_type)
+        return list(unique_job)
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
